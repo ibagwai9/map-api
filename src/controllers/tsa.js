73db62cc992003d22cc71bb97805bfb7eb54ef14
Adewale Muritala Akinyemi <adewalemurthador@gmail.com>
2023-06-24 11:20:25 +0100

    save

--- src/controllers/tsa.js
+++ src/controllers/tsa.js
@@ -21,3 +21,29 @@ exports.tsa_code = (req, res) => {
       error(err);
     });
 };
+
+export function kigra_get_account_list(req, res) {
+  const { query_type = null, head = null, search = null } = req.query;
+
+  db.sequelize
+    .query(`CALL kigra_account_list(:query_type,:head,:search)`, {
+      replacements: {
+        query_type,
+        head,
+        search,
+      },
+    })
+    .then((result) => {
+      res.json({
+        success: true,
+        result,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.json({
+        success: false,
+        err,
+      });
+    });
+}
--- src/routes/tsa.js
+++ src/routes/tsa.js
@@ -1,5 +1,6 @@
-import { tsa_code } from "../controllers/tsa";
+import { tsa_code, kigra_get_account_list } from "../controllers/tsa";
 
 module.exports = (app) => {
   app.get("/tsa-code", tsa_code);
+app.get("/kigra_get_account_list", kigra_get_account_list);
 };
