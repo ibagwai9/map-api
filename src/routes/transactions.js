698aa97ae14d0b37193ebed784a7b6f872edce63
ibagwai9 <ibagwai9@gmail.com>
2023-08-13 13:02:26 +0100

    changes

--- src/routes/transactions.js
+++ src/routes/transactions.js
@@ -2,11 +2,7 @@ import { getTrx, postTrx } from "../controllers/transactions";
 const {requireAuth} = require("../config/config.js")
 
   module.exports = (app) => {
-    
     app.post('/tansactions/execute', postTrx)
-         
-     app.get('/tansactions/retrieve', getTrx)
-
-         
+    app.get('/tansactions/retrieve', getTrx)
   };
   
