92bc0af119d5f5ee52f2ab9ec74283278f5fbe73
ibagwa9 <ibagwai9@gmail.com>
2023-09-05 23:19:24 +0100

    changes

--- src/models/index.js
+++ src/models/index.js
@@ -1,8 +1,8 @@
 'use strict';
 
-import fs  from 'fs';
-import path  from 'path';
-import Sequelize  from 'sequelize';
+const fs  = require ('fs');
+const path  = require ('path');
+const Sequelize  = require ('sequelize');
 require('dotenv').config();
 
 const basename = path.basename(__filename);
