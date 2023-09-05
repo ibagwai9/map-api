5ef09bf2d4c6e0845a5bb5c20275ea646f17492a
ibagwa9 <ibagwai9@gmail.com>
2023-09-05 01:03:45 +0100

    changes

--- src/Ishaq-Changes.sql
+++ src/Ishaq-Changes.sql
@@ -1,1 +1,8 @@
- const {  username=null, password=null, fullname=null,email=null, role='user', accessTo=null, bvn=null,company_name=null,rc=null,tin=null,account_type=null,phone=null,state=null,lga=null,address=null} = req.body;
+ALTER TABLE `presumptive_taxes` CHANGE `revenue_code` `economic_code` VARCHAR(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL, CHANGE `trade` `title` VARCHAR(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL;
+
+ALTER TABLE `presumptive_taxes` DROP PRIMARY KEY; 
+
+ALTER TABLE `presumptive_taxes` ADD `id` INT(6) NOT NULL AUTO_INCREMENT FIRST, ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `presumptive_taxes` ADD UNIQUE(`title`);
+
