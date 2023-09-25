37f01c899a0cac12927b1ba3a4fc487307ce6f88
Adewale Muritala Akinyemi <adewalemurthador@gmail.com>
2023-09-25 16:37:56 +0100

    save

--- src/config/passport.js
+++ src/config/passport.js
@@ -1,5 +1,6 @@
 const ExtractJwt = require("passport-jwt").ExtractJwt;
-const JwtStrategy = require("passport-jwt").Strategy;
+const ExtractJwt = require("passport-jwt").ExtractJwt;
+import { Strategy } from 'passsport-jwt';
 const db = require("../models");
 
 const opts = {};
