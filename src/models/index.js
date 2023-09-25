0391b0843e72310e50713be5c31f50181f2965c4
8790fahad <8790fahadado@gmail.com>
2023-09-25 14:01:10 +0100

    changes

--- src/models/index.js
+++ src/models/index.js
@@ -8,10 +8,10 @@ require("dotenv").config();
 const basename = path.basename(__filename);
 
 const config = {
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  host: process.env.DB_HOST,
+  username: process.env.DB_USERNAME||"root",
+  password: process.env.DB_PASSWORD||"",
+  database: process.env.DB_NAME ||"finance",
+  host: process.env.DB_HOST ||"localhost",
   port:   process.env.DB_PORT||3307,
   dialect:  "mysql",
   pool: {
