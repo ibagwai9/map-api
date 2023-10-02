1840607eba2f35e26ef7b072d8105eaf39b44438
8790fahad <8790fahadado@gmail.com>
2023-10-02 10:10:27 +0100

    changes

--- src/models/index.js
+++ src/models/index.js
@@ -12,7 +12,7 @@ const config = {
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   host: process.env.DB_HOST,
-  port:   process.env.DB_PORT,
+  // port:   process.env.DB_PORT,
   dialect:  process.env.DB_DIALECT,
   pool: {
     max: 5,
