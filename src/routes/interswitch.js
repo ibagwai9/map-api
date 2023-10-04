15dec4f10fcb4801f324264b0eab29d26598774f
ibagwa9 <ibagwai9@gmail.com>
2023-10-04 13:32:20 +0100

    cx

--- src/routes/interswitch.js
+++ src/routes/interswitch.js
@@ -5,7 +5,7 @@ module.exports = (app) => {
   app.post('/getTransaction',
   // passport.authenticate("jwt", { session: false }), 
   getTransaction);
-
   app.post('/invoice', handleInvoice)
   app.post('/lga-invoices', handleLgaInvoice)
+  app.post('/land-use-charges', handleLgaInvoice)
 }
