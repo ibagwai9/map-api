b0231920062d1d778b622e71860a9606b140cb00
Adewale Muritala Akinyemi <adewalemurthador@gmail.com>
2023-10-06 10:33:36 +0100

    save

--- src/controllers/segment.js
+++ src/controllers/segment.js
@@ -2,39 +2,43 @@ const moment = require("moment");
 const db = require("../models");
 const today = moment().format("YYYY-MM-DD");
 
-// module.exports =  postSegment = (req, res) => {
-//   const {
-//     segment_name="",segment_size ="",segmnt_type=""
-//   } = req.body;
-//   console.log(req.body);
-//   const {query_type=''} = req.query
-//   db.sequelize
-//     .query(
-//       `call segment (:segment_name,:segment_size,:segmnt_type,:query_type)`,
-//       {
-//         replacements: {
-//             segment_name,segment_size,segmnt_type,query_type,
-//         },
-//       }
-//     )
-//     .then((results) => {
-//       res.json({ success: true, results });
-//     })
-//     .catch((err) => {
-//       console.log(err);
-//       res.status(500).json({ success: false, err });
-//     });
-// };
+module.exports.postContactUs = (req, res) => {
+  const {
+    fullname = "",
+    email = "",
+    message = "",
+    insert_by = null,
+  } = req.body;
+  const { query_type = "insert" } = req.query;
+  db.sequelize
+    .query(
+      `call contact_us(:query_type, :fullname,:email,:message,:insert_by)`,
+      {
+        replacements: {
+          fullname,
+          email,
+          message,
+          query_type,
+          insert_by,
+        },
+      }
+    )
+    .then((results) => {
+      res.json({ success: true, results });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ success: false, err });
+    });
+};
 
 
 module.exports.getTaxPayer = (req, res) => {
-  const {query_type=''} = req.query
+  const { query_type = "" } = req.query;
   db.sequelize
-    .query(
-      `select * from tax_payers`
-    )
+    .query(`select * from tax_payers`)
     .then((results) => {
-      res.json({ success: true, results:results[0] });
+      res.json({ success: true, results: results[0] });
     })
     .catch((err) => {
       console.log(err);
--- src/routes/segment.js
+++ src/routes/segment.js
@@ -1,7 +1,16 @@
-const { getTaxPayer } = require("../controllers/segment");
+const { getTaxPayer, postContactUs } = require("../controllers/segment");
 const passport = require("passport");
 
 module.exports = (app) => {
   // app.post("/segment", postSegment);
-  app.get("/get-tax-payer",passport.authenticate("jwt", { session: false }), getTaxPayer);
+  app.get(
+    "/get-tax-payer",
+    passport.authenticate("jwt", { session: false }),
+    getTaxPayer
+  );
+  app.post(
+    "/post-contact-us",
+    passport.authenticate("jwt", { session: false }),
+    postContactUs
+  );
 };
