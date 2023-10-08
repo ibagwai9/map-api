db1c0c5f7d6c02d751e69135f0d39e3512afb506
8790fahad <8790fahadado@gmail.com>
2023-10-08 15:37:17 +0100

    changes

--- src/index.js
+++ src/index.js
@@ -102,6 +102,5 @@ require("./routes/budget.js")(app);
 var server = app.listen(port, function () {
   var host = server.address().address;
   var port = server.address().port;
-
   console.log("App listening at http://%s:%s", host, port);
 });
