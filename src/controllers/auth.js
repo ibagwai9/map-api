9fec12ded0c767a7f3cb5fa7cdf3a388bf7c6986
ibagwa9 <ibagwai9@gmail.com>
2023-10-11 11:37:00 +0100

    changes

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -909,7 +909,7 @@ module.exports.getTaxPayer = (req, res) => {
 
   // First, try to find the record in the tax_payers table
   db.sequelize
-    .query("SELECT * FROM tax_payers WHERE taxID=:user_id", {
+    .query(`SELECT * FROM tax_payers t WHERE t.taxID='${user_id}`, {
       replacements: {
         user_id,
       },
@@ -952,7 +952,7 @@ module.exports.getTaxPayers = (req, res) => {
 module.exports.getTaxPayerInfo = (req, res) => {
   const { user_id } = req.query;
   db.sequelize
-    .query(`SELECT * FROM tax_payers  WHERE taxID=:user_id`, {
+    .query(`SELECT * FROM tax_payers t  WHERE t.taxID=$'{user_id}'`, {
       replacements: {
         user_id,
       },
