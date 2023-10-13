702646889d7413219da4793b30f517472286fc23
ibagwa9 <ibagwai9@gmail.com>
2023-10-13 13:46:24 +0100

    changes

--- src/routes/interswitch.js
+++ src/routes/interswitch.js
@@ -1,10 +1,5 @@
-const {
-  getTransaction,
-  handleInvoice,
-  handleLgaInvoice,
-} = require("../controllers/interswitch");
+const { getTransaction } = require("../controllers/interswitch");
 const land = require("../controllers/land-interswitch");
-const tax = require("../controllers/tax-interswitch");
 const lga = require("../controllers/lga-interswitch");
 
 module.exports = (app) => {
@@ -13,9 +8,7 @@ module.exports = (app) => {
     // passport.authenticate("jwt", { session: false }),
     getTransaction
   );
-  app.post("/invoice", handleInvoice); //Tax/None tax, Motor Lisense
-  app.post("/tax/invoices", handleInvoice); //Tax/None tax, Motor Lisense
-  app.post("/nontax/invoices", handleInvoice); //Tax/None tax, Motor Lisense
+  app.post("/invoice", land.handleInvoice); //Tax/None tax, Motor Lisense
   app.post("/lga-invoices", lga.handleInvoice); // LGA INVOICE
   app.post("/land-use-charges", land.handleInvoice); // LAND INVOICE
 };
