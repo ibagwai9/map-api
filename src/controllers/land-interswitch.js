f26b631d4144d79c3661880c107d83cb412280a4
8790fahad <8790fahadado@gmail.com>
2023-10-25 14:54:47 +0100

    changes

--- src/controllers/land-interswitch.js
+++ src/controllers/land-interswitch.js
@@ -223,6 +223,7 @@ function formatIPv6MappedIPv4(ipv6MappedIPv4) {
 const allowedList = ["41.223.145.174", "154.72.34.174"];
 const handleInvoice = (req, res) => {
   const reqJson = req.body;
+  console.log(req);
   // const clientIP = req.ip;
   const clientIP = req.ip; // Get the client's IP address
   
