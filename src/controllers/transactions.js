7f29ace6970fe947e81d46c0cbbf10aa44e9e0c0
8790fahad <8790fahadado@gmail.com>
2023-11-21 09:09:34 +0100

    changes

--- src/controllers/transactions.js
+++ src/controllers/transactions.js
@@ -537,7 +537,7 @@ const printReport = (req, res) => {
     to = today,
     query_type = "",
   } = req.body;
-  const { sector } = req.query;
+  const { sector='' } = req.query;
   db.sequelize
     .query(
       `CALL print_report (:query_type, :ref_no, :user_id, :from, :to,:sector)`,
