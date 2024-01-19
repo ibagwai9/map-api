8e49309eac25b8c59e9fdd93daffaa09e74af077
ahmadkabir <atomakabir001@gmail.com>
2024-01-19 10:47:45 +0100

    save

--- src/controllers/reciept_logs.js
+++ src/controllers/reciept_logs.js
@@ -10,12 +10,14 @@ module.exports.reciept_logs = (req, res) => {
     staff_id = "",
     interswitch_ref_no = "",
     logId = "",
+    date_from = "",
+    date_to = "",
   } = req.body;
   console.log(req.body);
   const { query_type = "" } = req.query;
   db.sequelize
     .query(
-      `call reciept_logs (:query_type,:id,:ref_no,:status,:invoice_status,:remark,:staff_name,:staff_id,:interswitch_ref_no,:logId)`,
+      `call reciept_logs (:query_type,:id,:ref_no,:status,:invoice_status,:remark,:staff_name,:staff_id,:interswitch_ref_no,:logId,:date_from,:date_to)`,
       {
         replacements: {
           query_type,
@@ -28,6 +30,8 @@ module.exports.reciept_logs = (req, res) => {
           staff_id,
           interswitch_ref_no,
           logId,
+          date_from,
+          date_to,
         },
       }
     )
