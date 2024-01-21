9d2f91d0a7647e143c69459048e53245e56eb746
8790fahad <8790fahadado@gmail.com>
2024-01-21 13:54:01 +0100

    changes

--- src/models/index.js
+++ src/models/index.js
@@ -23,11 +23,10 @@ const basename = path.basename(__filename);
 // };
 
 const config = {
-  username: "root",
-  password: "",
-  database: 'new_kirmas',
-  host: "localhost",
-  port:   3307,  
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  host: process.env.DB_HOST,
   dialect: "mysql",
   pool: {
     max: 5,
