53bac9f4bbe87270f53707485c340c122bcbbeda
8790fahad <8790fahadado@gmail.com>
2024-01-22 11:41:21 +0100

    changes

--- src/controllers/transactions.js
+++ src/controllers/transactions.js
@@ -297,7 +297,7 @@ const getTrx = async (req, res) => {
     mda_val,
     sector,
     invoice_status,
-    tracking_status,
+    tracking_status: tracking_status ? tracking_status : "",
   };
 
   try {
