22d1550ceb00ae2a2ea415fc4e20e97598f397c8
8790fahad <8790fahadado@gmail.com>
2024-01-24 11:45:08 +0100

    changes

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -265,6 +265,7 @@ module.exports.SignIn = async (req, res) => {
       where: username.length > 10 ? { phone: username } : { taxID: username },
     });
     let users=[user.dataValues]
+    // let users=[user.dataValues]
     if (!users.length) {
       return res.status(404).json({
         success: false,
