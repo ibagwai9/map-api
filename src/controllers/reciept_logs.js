074ba6d534c7a1bddb68834492362ddc160194c6
8790fahad <8790fahadado@gmail.com>
2024-01-29 10:32:26 +0100

    changes

--- src/controllers/reciept_logs.js
+++ src/controllers/reciept_logs.js
@@ -12,6 +12,7 @@ module.exports.reciept_logs = (req, res) => {
     logId = "",
     date_from = null,
     date_to = null,
+    transactionData = [],
   } = req.body;
   console.log(req.body);
   const { query_type = "" } = req.query;
@@ -30,8 +31,8 @@ module.exports.reciept_logs = (req, res) => {
           staff_id,
           interswitch_ref_no,
           logId,
-          date_from,
-          date_to,
+          date_from: date_from || transactionData[0]?.date_from || null,
+          date_to: date_to || transactionData[0]?.date_to || null,
         },
       }
     )
@@ -41,5 +42,45 @@ module.exports.reciept_logs = (req, res) => {
     .catch((err) => {
       console.log(err);
       res.status(500).json({ success: false, err });
+    });
+};
+
+module.exports.getTransaction = (req, res) => {
+  const { reference_number = "" } = req.query;
+  db.sequelize
+    .query(
+      `SELECT  DISTINCT reference_number,tax_payer,GROUP_CONCAT(description," ") as description,dr,date_from,date_to,status  FROM tax_transactions t  WHERE t.reference_number='${reference_number}' and dr>0`,
+      {
+        replacements: {
+          reference_number,
+        },
+      }
+    )
+    .then((resp) => {
+      res.json({ success: true, data: resp[0] });
+    })
+    .catch((error) => {
+      console.error({ error });
+      res.status(500).json({ error, msg: "Error occurred" });
+    });
+};
+
+module.exports.getTransactionReq = (req, res) => {
+  const { tracking_status = "" } = req.query;
+  db.sequelize
+    .query(
+      `SELECT  DISTINCT reference_number,tax_payer,GROUP_CONCAT(description," ") as description,dr,date_from,date_to,status FROM tax_transactions   where tracking_status=:tracking_status and  dr>0 GROUP BY reference_number`,
+      {
+        replacements: {
+          tracking_status,
+        },
+      }
+    )
+    .then((resp) => {
+      res.json({ success: true, data: resp[0] });
+    })
+    .catch((error) => {
+      console.error({ error });
+      res.status(500).json({ error, msg: "Error occurred" });
     });
 };
--- src/routes/reciept_logs.js
+++ src/routes/reciept_logs.js
@@ -1,9 +1,21 @@
 const passport = require("passport");
-const { reciept_logs } = require("../controllers/reciept_logs");
+const { reciept_logs, getTransaction, getTransactionReq } = require("../controllers/reciept_logs");
 module.exports = (app) => {
   app.post(
     "/reciept_logs",
     passport.authenticate("jwt", { session: false }),
     reciept_logs
   );
+  app.get(
+    "/adjust-transactions",
+    passport.authenticate("jwt", { session: false }),
+    getTransaction
+  );
+  app.get(
+    "/get-trans-req",
+    passport.authenticate("jwt", { session: false }),
+    getTransactionReq
+  );
+  
+  
 };
