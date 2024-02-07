a157ad4d23e0a6a21b7629e617d58017eb545ac6
8790fahad <8790fahadado@gmail.com>
2024-02-07 08:53:38 +0100

    changes

--- src/routes/reciept_logs.js
+++ src/routes/reciept_logs.js
@@ -1,11 +1,29 @@
 const passport = require("passport");
-const { reciept_logs, getTransaction, getTransactionReq } = require("../controllers/reciept_logs");
+const { reciept_logs, getTransaction, getTransactionReq, getRemarks, reciept_logs_up, rejectReq } = require("../controllers/reciept_logs");
 module.exports = (app) => {
   app.post(
     "/reciept_logs",
     passport.authenticate("jwt", { session: false }),
     reciept_logs
   );
+  
+  app.post(
+    "/reject-req",
+    passport.authenticate("jwt", { session: false }),
+    rejectReq
+  );
+  app.post(
+    "/reciept_logs_up",
+    passport.authenticate("jwt", { session: false }),
+    reciept_logs_up
+  );
+  
+  app.get(
+    "/get-remarks",
+    passport.authenticate("jwt", { session: false }),
+    getRemarks
+  );
+  
   app.get(
     "/adjust-transactions",
     passport.authenticate("jwt", { session: false }),
