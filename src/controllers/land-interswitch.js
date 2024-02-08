bc4ff0e08841007d3b2cfbdd4543bcb094aa6961
8790fahad <8790fahadado@gmail.com>
2024-02-08 10:40:30 +0100

    changes

--- src/controllers/land-interswitch.js
+++ src/controllers/land-interswitch.js
@@ -145,7 +145,7 @@ const handleInvoiceValidation = async (reqJson, res) => {
             <Customer>
                 <Status>0</Status>
                 <CustReference>${custreference}</CustReference>
-                <FirstName>${firstName?.replace("&", "&amp;")?.replace("'", "&rsquo;")}</FirstName>
+                <FirstName>${firstName?.replace("&", "&amp;")?.replace("'", "")}</FirstName>
                 <Phone>${results[0].phone}</Phone>
                 <Amount>${amount}</Amount>
                 ${xmlString}
